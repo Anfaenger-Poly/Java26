d120b13a768f2663f9750c26721085ce573ef8d4
Anfaenger-Poly <seck999@gmail.com>
2026-08-19 11:48:19 +0900

    Add files via upload

---
+++ day01/Hello.java
@@ -0,0 +1,14 @@
+package day01;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello!! Java");
+		char ch1 = 89;
+		char ch2 = 'Y';
+		System.out.println(ch1);
+		System.out.println(ch2);
+
+	}
+
+}
